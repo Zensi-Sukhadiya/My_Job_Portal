ab2fcc992edb42ae839da00d79d9d08b025c7885
Zensi-Sukhadiya <sukhadiyazensi26@gmail.com>
2026-06-26 11:09:47 +0530

    deploy django to vercel

--- My_Job_Portal/settings.py
+++ My_Job_Portal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w!oz=$3c4fb)-_w9)pbf8tt_668c#=owf169cc=lblad=69tj&'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
